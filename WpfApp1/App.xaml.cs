704df7dfeaa886b7fa1d3b119ce1bea110c836d4
GUSTAVO SANTOS GUIMARAES <081180013@faculdade.cefsa.edu.br>
2022-11-16 20:15:37 -0300

    inicia tela de login

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WpfApp1.Views;
 
 namespace WpfApp1
 {
@@ -13,5 +14,13 @@ namespace WpfApp1
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+        }
     }
 }
